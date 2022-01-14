5325a9b1742c9e5cb1dc1dae0a031374bf966094
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-01-14 20:33:04 +0530

    finished app & firebase setting up process

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZoUH0vhrcTp5DAXCKpQ7zOBU_hJ7WxwQ",
+  authDomain: "clone-1-ca481.firebaseapp.com",
+  projectId: "clone-1-ca481",
+  storageBucket: "clone-1-ca481.appspot.com",
+  messagingSenderId: "387765813711",
+  appId: "1:387765813711:web:ad9050fc9346b4dc55d2ba"
+};
